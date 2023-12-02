ac82591153a8759f457eb1f0fe2dc2691f3f3174
jaseelamahar <jaseelamahar@outlook.com>
2023-12-02 14:28:26 +0530

    task2

--- dom.js
+++ dom.js
@@ -26,5 +26,14 @@ headerTitle.innerHTML='<h3>Hello</h3>'
 header.style.borderBottom='solid 3px #000'
 
 const element=document.querySelector('.title');
-element.style.color='green';
+element.style.color='blue';
 element.style.fontWeight='bold';
+
+var items=document.getElementsByClassName("list-group-item");
+console.log(items);
+console.log(items[2]);
+items[2].style.backgroundColor="green";
+
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight="bold";
+}
