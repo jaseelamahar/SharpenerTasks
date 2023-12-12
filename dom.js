9348bea726ccb361f7d7a5e1c13d8e7b261a1ff5
jaseelamahar <jaseelamahar@outlook.com>
2023-12-12 18:17:14 +0530

    filter added

--- dom.js
+++ dom.js
@@ -2,17 +2,24 @@
 
 var form=document.getElementById("addForm");
 var itemList=document.getElementById("items");
+var filter=document.getElementById("filter");
+
+
+
+
  //for submit event
  form.addEventListener('submit',addItem);
  // Delete event
 itemList.addEventListener('click', removeItem);
+//filter event
+filter.addEventListener('keyup',filterItems);
+ 
+
 function addItem(e) {
   e.preventDefault();
-
-  // Get input value
+ // Get input value
   var newItem = document.getElementById('item').value;
-
-  // Create new li element
+ // Create new li element
   var li = document.createElement('li');
   // Add class
   li.className = 'list-group-item';
@@ -61,3 +68,22 @@ function removeItem(e){
    }
  }
 }
+ //filter Items
+ function filterItems(e){
+  //convert text to lowercase
+  var text=e.target.value.toLowerCase();
+  //Get list
+  var items=itemList.getElementsByTagName('li');
+//console.log(items)
+Array.from(items).forEach(function(item){
+  var itemName=item.firstChild.textContent;
+  //console.log(itemName);
+  if(itemName.toLowerCase().indexOf(text)!=-1){
+    item.style.display="block";
+  }
+  else{
+    item.style.display='none';
+  }
+})
+
+ }
